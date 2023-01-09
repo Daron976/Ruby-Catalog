07b2ea284548cfb4b41158ff936e90081e1e4a83
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-09 16:43:36 +0300

    added creating book function

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
+require './label'
+require './book'
+require_relative './data/book_data'
+
 class App
+  def initialize
+    @all_books = []
+    @all_labels = []
+  end
+
   def options
     puts "
     1 - List all book
@@ -27,6 +36,7 @@ class App
     case user_input
     when 1
       puts 'List of books:'
+      list_books
     when 2
       puts 'List of albums:'
     when 3
@@ -35,22 +45,72 @@ class App
       puts 'List of genres:'
     when 5
       puts 'List of labels:'
+      list_labels
     when 6
       puts 'List of authors:'
     when 7
       puts 'List of sources:'
     when 8
       puts 'Add a book'
+      create_book
     when 9
       puts 'Add a music album'
     when 10
       puts 'Add a movie'
     when 11
-      Exit
+      exit
     else
       'Wrong input!'
     end
+    options
   end
   # rubocop:enable Metrics/CyclomaticComplexity
   # rubocop:enable Metrics/MethodLength
+
+  def create_book
+    print 'Publisher:'
+    book_publisher = gets.chomp
+
+    print 'Cover State:'
+    book_cover_state = gets.chomp
+
+    print 'Label Title:'
+    book_label_title = gets.chomp
+
+    print 'Label Color:'
+    book_label_color = gets.chomp
+
+    print 'Publish Date(DD-MM-YYYY):'
+    book_publish_date = gets.chomp
+
+    mylabel = Label.new book_label_title, book_label_color
+    mybook = Book.new book_publisher, book_cover_state, book_publish_date
+
+    @all_books << mybook
+    @all_labels << mylabel
+
+    mylabel.add_item(mybook)
+    save_book(book_publisher, book_cover_state, book_label_title, book_label_color, book_publish_date)
+
+    puts 'Book created.'
+  end
+
+  def list_books
+    puts 'No book recordssss.' if @all_books.empty?
+
+    @all_books.each do |book|
+      puts "ID:#{book.id}"
+      puts "Publisher:#{book.publisher}"
+      puts "Cover State:#{book.cover_state}"
+      puts "Label:'#{book.label.title}', '#{book.label.color}'"
+      puts "Publish date:#{book.publish_date}"
+      puts
+    end
+  end
+
+  def list_labels
+    puts 'No label recordsss.' if @all_labels.empty?
+
+    @all_labels.each { |label| puts "Id: '#{label.id}', Title: '#{label.title}', Color: '#{label.color}'" }
+  end
 end
--- data/book_data.rb
+++ data/book_data.rb
@@ -1,3 +1,38 @@
 require_relative '../book'
 require_relative '../label'
 require 'json'
+
+def load_labels_books(mybook, mylabel)
+  if !File.exist?('./data/books.json') || File.size('./data/books.json').zero?
+    mybook << []
+    mylabel << []
+    return
+  end
+
+  books = JSON.parse(File.read('./data/books.json'))
+  books.each do |book|
+    label = Label.new(book['book_label_title'], book['book_label_color'])
+    book = Book.new(book['book_publisher'], book['book_cover_state'], book['book_publish_date'])
+    mybook << book
+    mylabel << label
+    label.add_item(book)
+  end
+end
+
+def save_book(book_publisher, book_cover_state, book_label_title, book_label_color, book_publish_date)
+  obj = {
+    book_publisher: book_publisher,
+    book_cover_state: book_cover_state,
+    book_label_title: book_label_title,
+    book_label_color: book_label_color,
+    book_publish_date: book_publish_date
+  }
+
+  book = if !File.exist?('./data/books.json') || File.size('./data/books.json').zero?
+           [obj]
+         else
+           JSON.parse(File.read('./data/books.json')) << obj
+         end
+
+  File.write('./data/books.json', JSON.pretty_generate(book))
+end
