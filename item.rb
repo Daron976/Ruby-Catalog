10627223ad00838276eeee497ad2a233e0d57a27
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-09 12:15:01 +0300

    move to archive method added

--- item.rb
+++ item.rb
@@ -16,8 +16,12 @@ class Item
   private
 
   def can_be_archived?
-  age = Time.now.year - @publish_date.year
-  age > 10
+    age = Time.now.year - @publish_date.year
+    age > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
   end
 
 end
