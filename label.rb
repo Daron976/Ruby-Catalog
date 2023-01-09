71430e16add9d0296daf0d91d1f451479b6fb907
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-09 14:15:35 +0300

    added label class

---
+++ label.rb
@@ -0,0 +1,17 @@
+require_relative 'item'
+
+class Label
+  attr_accessor :id, :title, :color, :items
+
+  def initialize(title, color)
+    @id = Random.rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.add_label(self)
+  end
+end
