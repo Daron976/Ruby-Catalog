8a3ffa721186197fbb805226edb3bf1f0d7315d8
Daron976 <daronkwiafe@gmail.com>
2023-01-09 11:09:41 +0200

    Initialize item class

---
+++ item.rb
@@ -0,0 +1,15 @@
+class Item
+  attr_accessor :genre, :author, :source, :label, :publish_date, :archived
+
+  attr_reader :id
+
+  def initialize(genre, author, source, label, date, archived: false)
+    @id = Random.rand(1...1000)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = date
+    @archived = archived
+  end
+end
