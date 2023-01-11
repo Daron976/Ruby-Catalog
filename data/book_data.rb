30af60559283e82e6b0c7084a58858d30785d77b
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-11 15:08:18 +0300

    methods added to item class

--- data/book_data.rb
+++ data/book_data.rb
@@ -1,3 +1,52 @@
 require_relative '../book'
 require_relative '../label'
 require 'json'
+
+
+def load_label_and_books(mybook, mylabel)
+  if File.exist?('./data/books.json')
+    file = File.open('./data/books.json')
+    if file.size.zero?
+      mybook << []
+      mylabel << []
+    else
+      books = JSON.parse(File.read('./data/books.json'))
+      books.each do |book|
+        label = Label.new(book['book_label_title'], book['book_label_color'])
+        book = Book.new(book['book_publisher'], book['book_cover_state'], book['book_publish_date'])
+        mybook << book
+        mylabel << label
+        label.add_item(book)
+      end
+    end
+    file.close
+  else
+    mybook << []
+    mylabel << []
+  end
+end
+
+def save_book(book_publisher, book_cover_state, book_label_title, book_label_color, book_publish_date)
+  obj = {
+
+    book_publisher: book_publisher,
+    book_cover_state: book_cover_state,
+    book_label_title: book_label_title,
+    book_label_color: book_label_color,
+    book_publish_date: book_publish_date
+
+  }
+
+  return unless File.exist?('./data/books.json')
+  file = File.open('./data/books.json')
+  if file.size.zero?
+    book = [obj]
+  else
+    book = JSON.parse(File.read('./data/books.json'))
+    book << obj
+  end
+  file.close
+  myfile = File.open('./data/books.json', 'w')
+  myfile.write(JSON.pretty_generate(book))
+  myfile.close
+end
--- item.rb
+++ item.rb
@@ -13,6 +13,21 @@ class Item
     @archived = false
   end
 
+  def add_genre(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
+  end
+
   private
 
   def can_be_archived?
